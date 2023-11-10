67c411dba0cf020b5f74de557d60fd950259592e
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-11-10 14:36:35 +0000

    apply code formatting

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -1,19 +1,21 @@
 module Cipher where
+
 import Data.Char
 
 -- Caeser Cipher
 caeserCipher :: String -> Int -> String
 caeserCipher [] _ = []
-caeserCipher (x:xs) n = (shiftChar x n) : (caeserCipher xs n) where
+caeserCipher (x : xs) n = (shiftChar x n) : (caeserCipher xs n)
+  where
     shiftLower :: Char -> Int -> Char
     shiftLower c n = chr $ (ord c - ord 'a' + n) `mod` 26 + ord 'a'
     shiftUpper :: Char -> Int -> Char
     shiftUpper c n = chr $ (ord c - ord 'A' + n) `mod` 26 + ord 'A'
     shiftChar :: Char -> Int -> Char
     shiftChar c n
-        | isLower c = shiftLower c n
-        | isUpper c = shiftUpper c n
-        | otherwise = c
+      | isLower c = shiftLower c n
+      | isUpper c = shiftUpper c n
+      | otherwise = c
 
 -- Vigenère Cipher
 -- Morse Code
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,8 @@
+import Cipher
 import Data.Char
 import Story
-import Cipher
 
 main = do
-    tellStory storyEinleitung
-    input <- getLine
-    putStrLn $ if input == "Willkommen im Spiel" then "Richtig" else "Falsch"
+  tellStory storyEinleitung
+  input <- getLine
+  putStrLn $ if input == "Willkommen im Spiel" then "Richtig" else "Falsch"
